4337f86d963e7bf0cd9f2c4157bf9b9fc82070b7
Yigal Irani <yigal@symbolclick.com>
2021-12-17 11:16:38 -0500

    marking range in green works!!1

--- src/taskpane.js
+++ src/taskpane.js
@@ -1,33 +1,14 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global console, document, Excel, Office */
-
-// The initialize function must be run each time a new page is loaded
 Office.initialize = () => {
-
   document.getElementById("run").onclick = run;
 };
-
-export async function run() {
+function mark_range(context){
+  const range = context.workbook.getSelectedRange();
+  range.format.fill.color = "green";
+  return context.sync();
+}
+export function run() {
   try {
-    await Excel.run(async (context) => {
-      /**
-       * Insert your Excel code here
-       */
-      const range = context.workbook.getSelectedRange();
-
-      // Read the range address
-      range.load("address");
-
-      // Update the fill color
-      range.format.fill.color = "yellow";
-
-      await context.sync();
-      console.log(`The range address was ${range.address}.`);
-    });
+    Excel.run(mark_range)
   } catch (error) {
     console.error(error);
   }
