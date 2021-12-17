fef75b1634f9aeb91e8c3f2aefa522ec202480ec
Yigal Irani <yigal@symbolclick.com>
2021-12-17 03:07:52 -0500

    simplification, starts working

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 /* eslint-disable no-undef */
 
-const devCerts = require("office-addin-dev-certs");
+
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const CustomFunctionsMetadataPlugin = require("custom-functions-metadata-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 
 
 const urlDev = "http://localhost/";
@@ -12,10 +11,6 @@ const urlProd = "https://symbolclick.com/addon/"; // CHANGE THIS TO YOUR PRODUCT
 
 /* global require, module, process, __dirname */
 
-async function getHttpsOptions() {
-  const httpsOptions = await devCerts.getHttpsServerOptions();
-  return { cacert: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
-}
 
 module.exports = async (env, options) => {
   const dev = options.mode === "development";
@@ -23,17 +18,9 @@ module.exports = async (env, options) => {
   const config = {
     devtool: "source-map",
     entry: {
-      polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-      functions: "./src/functions/functions.js",
-      taskpane: "./src/taskpane/taskpane.js",
-      commands: "./src/commands/commands.js",
-    },
-    output: {
-      devtoolModuleFilenameTemplate: "webpack:///[resource-path]?[loaders]",
-      clean: true,
-    },
-    resolve: {
-      extensions: [".ts", ".tsx", ".html", ".js"],
+      functions: "./src/functions.js",
+      taskpane: "./src/taskpane.js",
+      commands: "./src/commands.js",
     },
     module: {
       rules: [
@@ -41,47 +28,23 @@ module.exports = async (env, options) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader", 
-            options: {
-              presets: ["@babel/preset-env"],
-            },
-          },
-        },
-        {
-          test: /\.html$/,
-          exclude: /node_modules/,
-          use: "html-loader",
-        },
-        {
-          test: /\.(png|jpg|jpeg|gif|ico)$/,
-          type: "asset/resource",
-          generator: {
-            filename: "assets/[name][ext][query]",
+            loader: "babel-loader"
           },
         },
       ],
     },
     plugins: [
-      new CustomFunctionsMetadataPlugin({
-        output: "functions.json",
-        input: "./src/functions/functions.js",
-      }),
-      new HtmlWebpackPlugin({
-        filename: "functions.html",
-        template: "./src/functions/functions.html",
-        chunks: ["polyfill", "functions"],
-      }),
-      new HtmlWebpackPlugin({
-        filename: "taskpane.html",
-        template: "./src/taskpane/taskpane.html",
-        chunks: ["polyfill", "taskpane"],
-      }),
+
       new CopyWebpackPlugin({
         patterns: [
           {
-            from: "assets/icon-*",
+            from: "assets/*",
             to: "assets/[name][ext][query]",
           },
+          { 
+            from : "src/*.(html|css|json)",
+            to :"[name][ext]"
+          },              
           {
             from: "manifest*.xml",
             to: "[name]." + buildType + "[ext]",
@@ -95,21 +58,10 @@ module.exports = async (env, options) => {
           },
         ],
       }),
-      new HtmlWebpackPlugin({
-        filename: "commands.html",
-        template: "./src/commands/commands.html",
-        chunks: ["polyfill", "commands"],
-      }),
-    ],
-    devServer: {
-      static: [__dirname],
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-      https: env.WEBPACK_BUILD || options.https !== undefined ? options.https : await getHttpsOptions(),
-      port: process.env.npm_package_config_dev_server_port || 3000,
-    },
-  };
+ 
+    ]
+
+}
 
   return config;
 };
